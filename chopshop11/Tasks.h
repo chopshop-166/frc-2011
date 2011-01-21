96dbb53bf7e5ed3ba0df40b04cbff7895c93ae07
chopshop <166.first@gmail.com>
2011-01-20 20:10:16 -0500

    Added # define for suitcase bot. Initialize objects that need jaguars in the "cannot run on suitcase" area.

--- chopshop11/Tasks.h
+++ chopshop11/Tasks.h
@@ -4,4 +4,16 @@
 // Example:
 // DriveClass DriveObject;
 // No need for pointers, or anything else, if it's derived from Team166Task
+#define SUITCASE_BOT 0
+
+// tasks that run everywhere
+
+
+#if not SUITCASE_BOT
+// tasks that cannot run on suitcase
 DriveTask DriveObject;
+
+#else
+//suitcase only tasks here
+
+#endif
