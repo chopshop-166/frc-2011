51de2906b1aa5a71be6678871291940c701c374f
M <devnull@localhost>
2011-02-04 19:00:31 -0500

    Fiddling with HG...

--- chopshop11/TargetLight.cpp
+++ chopshop11/TargetLight.cpp
@@ -1,9 +1,10 @@
 /*******************************************************************************
-*  Project   		: Framework
-*  File Name  		: CameraTask.cpp     
+*  Project   		: Image Processing
+*  File Name  		: TargetLight.cpp     
 *  Owner		   	: Software Group (FIRST Chopshop Team 166)
-*  Creation Date	: January 18, 2010
-*  File Description	: Template source file for tasks, with template functions
+*  Creation Date	: February 2, 2011
+*  File Description	: Returns a normalized number that says to go left or right
+* 					  based on the image given.
 *******************************************************************************/ 
 /*----------------------------------------------------------------------------*/
 /*  Copyright (c) MHS Chopshop Team 166, 2010.  All Rights Reserved.          */
@@ -24,10 +25,34 @@
 #include "TrackAPI.h"
 
 #define CAMERA_OFFSET 0.0
-
 // To locally enable debug printing: set true, to disable false
 #define DPRINTF if(false)dprintf
 
+
+
+//Converts the camera image into a binary image with targets highlighted
+void IsolateLightTarget(Image* ReflectingTape, Image* srcimage)
+{
+//Create binary image with threshold technique
+	Range R_Range;
+	Range B_Range;
+	Range G_Range;
+	//Define the RGB minimum values
+		R_Range.minValue = 100;
+		G_Range.minValue = 100;
+		B_Range.minValue = 100;
+		R_Range.maxValue = 255;
+		G_Range.maxValue = 255;
+		B_Range.maxValue = 255;	
+	frcColorThreshold(ReflectingTape, srcimage, IMAQ_RGB, &R_Range, &G_Range, &B_Range);
+	
+//Edit the binary image that has targets highlighted for readability
+	//int imaqSizeFilter(ReflectingTape, ReflectingTape, 1, 0, IMAQ_KEEP_LARGE, NULL); 
+
+
+}
+
+//This function is WPI's, but it instead searches for the widest particle
 int GetWidestParticle(Image* binaryImage, int* widestParticleIndex)
 {
 	*widestParticleIndex = 0; 
@@ -58,91 +83,47 @@ int GetWidestParticle(Image* binaryImage, int* widestParticleIndex)
 };
 
 
+
 int ProcessTheImage(Image* srcimage, double* targetCenterNormalized)  
 {
-	
 //Defining the needed variables
-	//Image holders:
-		Image* ReflectingTape = frcCreateImage(IMAQ_IMAGE_U8);
-	//Integers:
-		int widestParticleIndex = 0;
-	//Other variables:
-		ParticleAnalysisReport Biggest;
-		Range R_Range;
-		Range B_Range;
-		Range G_Range;
-		//Define the RGB minimum values
-			R_Range.minValue = 100;
-			G_Range.minValue = 100;
-			B_Range.minValue = 100;
-			R_Range.maxValue = 255;
-			G_Range.maxValue = 255;
-			B_Range.maxValue = 255;
+	Image* ReflectingTape = frcCreateImage(IMAQ_IMAGE_U8);
+	int widestParticleIndex = 0;
+	ParticleAnalysisReport Biggest;
 	
-//The algorithm:
-	//These transform the source image (srcimage) into a binary image that has only the targets as particles
-			frcColorThreshold(ReflectingTape, srcimage, IMAQ_RGB, &R_Range, &G_Range, &B_Range);
+//Making the image usable
+	//Transform the camera image into a binary image (Reflecting Tape) with only the targets as "on"
+	IsolateLightTarget(ReflectingTape, srcimage); 
 	
-	//logic
-	
+//Using usable image to return 
+	//Determine the wanted target: the central circle/the central circle and its strip-friends
 	int success = GetWidestParticle(ReflectingTape, &widestParticleIndex);
-	
-	if(!success) 
-	{
-		frcParticleAnalysis(ReflectingTape, widestParticleIndex, &Biggest);
-		return 1;
-	} 
-	else
-	{
-		return 0;
-	}
-		
+	//If it worked, analyze the particle
+	if(!success) {frcParticleAnalysis(ReflectingTape, widestParticleIndex, &Biggest); return 1;} 
+	else {return 0;}
+	//Return the normalized center - points out which way to turn, accounting for camera's offset (if any)
 	*targetCenterNormalized = Biggest.center_mass_x_normalized - CAMERA_OFFSET;
 };
 
 int ProcessTheImage(Image* srcimage, double* targetCenterNormalized, Image* ColoredBinaryImage, ImageType* type)  
 {
-	
 //Defining the needed variables
-	//Image holders:
-		Image* ReflectingTape = frcCreateImage(IMAQ_IMAGE_U8);
-	//Integers:
-		int widestParticleIndex = 0;
-	//Other variables:
-		ParticleAnalysisReport Biggest;
-		Range R_Range;
-		Range B_Range;
-		Range G_Range;
-		//Define the RGB minimum values
-			R_Range.minValue = 100;
-			G_Range.minValue = 100;
-			B_Range.minValue = 100;
-			R_Range.maxValue = 255;
-			G_Range.maxValue = 255;
-			B_Range.maxValue = 255;
+	Image* ReflectingTape = frcCreateImage(IMAQ_IMAGE_U8);
+	int widestParticleIndex = 0;
+	ParticleAnalysisReport Biggest;
 	
-//The algorithm:
-	//These transform the source image (srcimage) into a binary image that has only the targets as particles
-			frcColorThreshold(ReflectingTape, srcimage, IMAQ_RGB, &R_Range, &G_Range, &B_Range);
-			imaqCast(ColoredBinaryImage, ReflectingTape, *type, NULL, -1); 
-
+//Making the image usable
+	//Transform the camera image into a binary image (Reflecting Tape) with only the targets as "on"
+	IsolateLightTarget(ReflectingTape, srcimage); 
+	//Return the binary image as the requested type for testing
+	imaqCast(ColoredBinaryImage, ReflectingTape, *type, NULL, -1); 
 	
-	//logic
-	
+//Using usable image to return 
+	//Determine the wanted target: the central circle/the central circle and its strip-friends
 	int success = GetWidestParticle(ReflectingTape, &widestParticleIndex);
-	
-	if(!success) 
-	{
-		frcParticleAnalysis(ReflectingTape, widestParticleIndex, &Biggest);
-		return 1;
-	} 
-	else
-	{
-		return 0;
-	}
-		
+	//If it worked, analyze the particle
+	if(!success) {frcParticleAnalysis(ReflectingTape, widestParticleIndex, &Biggest); return 1;} 
+	else {return 0;}
+	//Return the normalized center - points out which way to turn, accounting for camera's offset (if any)
 	*targetCenterNormalized = Biggest.center_mass_x_normalized - CAMERA_OFFSET;
 };
-
-
-//This code was copied from WPI's library, but all instances of "large" were replaced with "wide"
