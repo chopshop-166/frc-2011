a2c2fb91931d89a22b95a5360576845cd2858740
Matt Soucy <demosthenes2k7@gmail.com>
2011-03-03 20:43:39 -0500

    Enabled most of the tasks in preparation for tommorrow

--- chopshop11/Tasks.h
+++ chopshop11/Tasks.h
@@ -8,11 +8,11 @@
 //TimerTask TimerTaskObject;
 
 // tasks that cannot run on suitcase
-//ArmTask ArmTaskObject; // Works, but not logging faults
+ArmTask ArmTaskObject; // Works, but not logging faults
 //AutonomousAssistTask AutoAssistObject; // On vacation...do not disturb 
-//ColorLightTask ColorLightObject; // Works
-//DriveTask DriveObject; // Works
-//ElevatorTask ElevatorObject; // Works, but not logging faults
+ColorLightTask ColorLightObject; // Works
+DriveTask DriveObject; // Works
+ElevatorTask ElevatorObject; // Works, but not logging faults
 MiniDeploy166 MiniDeployObject; // In progress
 //PhotoElectricTask PhotoElectricObject; // Works
 PneumaticsTask PneumaticsTaskObject; // Works
